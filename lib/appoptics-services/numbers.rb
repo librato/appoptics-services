ca14751ed190e66646fdc68d02bf9a10b10a92bd
Oskar Kapusta <oskar@librato.com>
2019-07-02 13:32:19 +0200

    Add number_with_delimiter helper to format numbers to be more readable

--- lib/appoptics-services/numbers.rb
+++ lib/appoptics-services/numbers.rb
@@ -1,3 +1,5 @@
+require 'active_support/all'
+
 module AppOptics
   module Services
     class Numbers
@@ -6,11 +8,11 @@ module AppOptics
         number_decimals = number_decimal_places(number)
 
         if !threshold_decimals || !number_decimals
-          return number
+          return number_with_delimiter(number)
         end
 
         if (number_decimals - tolerance) <= threshold_decimals
-          return number
+          return number_with_delimiter(number)
         end
 
         # here we have more decimals in the number than the threshold
@@ -18,7 +20,7 @@ module AppOptics
         # threshold: 3.14
 
         factor = (10**(threshold_decimals+tolerance)).to_f
-        (number * factor).truncate / factor
+        number_with_delimiter((number * factor).truncate / factor)
       end
 
       def self.number_decimal_places(number)
@@ -28,7 +30,27 @@ module AppOptics
         end
         segments[1].length
       end
+
+      # File actionpack/lib/action_view/helpers/number_helper.rb, line 199
+      def self.number_with_delimiter(number, options = {})
+        options.symbolize_keys!
+
+        begin
+          Float(number)
+        rescue ArgumentError, TypeError
+          if options[:raise]
+            raise InvalidNumberError, number
+          else
+            return number
+          end
+        end
+
+        options = options.reverse_merge({ delimiter: ',', separator: '.' })
+
+        parts = number.to_s.to_str.split('.')
+        parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{options[:delimiter]}")
+        parts.join(options[:separator]).html_safe
+      end
     end
   end
 end
-
